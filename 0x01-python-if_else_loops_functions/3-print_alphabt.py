1535a92771c673a063d550eb425d57533c0614d5
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-05-10 21:32:56 +0300

    print alpha except

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-   if chr(i) != "q" and chr(i)!="e":
-        print("{:c}".format(i), end ="")
+for i in range(ord('a'), ord('z')+1):
+    if i in [101, 113]:
+        continue
+    print("{:c}".format(i), end="")
