13bd34831636a302f8eddecb3bf046571035aacd
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-05-10 23:27:59 +0300

    A program that prints numbers from 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for num1 in range(0, 8):
-    for num2 in range(num1 + 1, 10):
-        print("{:d}{:d}".format(num1, num2), end=", ")
-print("{:d}{:d}".format(num1 + 1, num2))
+for numbers in range(00, 99):
+    print("{:02d}".format(numbers), end=", ")
+print("{:02d}".format(numbers + 1))
