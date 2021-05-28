8209efc11647d2f7a0446091a484fb05449ea644
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-05-28 22:08:48 +0300

    Create 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" function that prints My name is <first name> <last name> """
+
+
+def say_my_name(first_name, last_name=""):
+    """ function that prints a string"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
