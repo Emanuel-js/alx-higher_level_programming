6a9b7a8c04622bf3e7a976bd0fa7b32e9f861a4c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-06-01 19:21:58 +0300

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initial attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary """
+        if attrs is not None:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ defines a student """
+        for key, value in json.items():
+            self.__dict__[key] = value
