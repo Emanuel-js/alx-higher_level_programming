106bf512ca24d0aeeb89f05d6fda3b79934e8d2c
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 15:04:48 +0300

    Create 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+  
+#!/usr/bin/python3
+"""
+Definition of module
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """initializer
+        Args
+           width
+           height
+        """
+        if super().integer_validator('width', width):
+            self.__width = width
+        if super().integer_validator('height', height):
+            self.__height = height
