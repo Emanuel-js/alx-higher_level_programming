a9dffd1ac55ec7ec8d614f096c9c08868af25401
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 14:57:10 +0300

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Definition of module
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks if object is exactly an instance of a_class
+    Args
+       obj: object
+       a_class: class
+    """
+    return(type(obj) == a_class)
