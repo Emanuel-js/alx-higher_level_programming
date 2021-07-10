bde3cf02d0aef73243ae5392228f98bd32dfb72d
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 14:59:04 +0300

    Create 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Definition of module
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if object is an instance of a class inherited from a_class
+    Args
+        obj: object
+        a_class: class
+    Return: True or False"""
+    if not type(obj) == a_class and issubclass(type(obj), a_class):
+        return(True)
+    else:
+        return(False)
