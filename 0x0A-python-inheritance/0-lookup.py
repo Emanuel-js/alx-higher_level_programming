afa00d759ea48589b390d82bc7df7f8d8d0d6e92
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-10 14:48:08 +0300

    list of available attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns list of available attributes & methods of an object"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
