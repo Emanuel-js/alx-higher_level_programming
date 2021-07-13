8f7be0483e86836397e772aa595f5e4bf9b43001
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-13 20:14:58 +0300

    create a data base

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0c_0 in my MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+Create Database IF NOT EXISTS hbtn_0c_0;
