888bb406ddd8da65ce5b82a34aa252ccd36253a5
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-07-14 19:40:37 +0300

    inset a new value

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,10 @@
+-- A cript that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
+-- New row:
+-- id = 89
+-- name = Holberton School
+-- The database name will be passed as an argument of the mysql command.
+INSERT INTO first_table (id,
+name)
+VALUES(
+89,
+'Holberton School');
