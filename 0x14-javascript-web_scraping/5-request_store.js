87f0202598503363ea335873e9eac136fcd7b143
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-14 21:53:31 +0300

    Create 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+	#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+request.get(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  fs.writeFile(process.argv[3], body, 'utf-8', function (err, data) {
+    if (err) throw err;
+  });
+});
