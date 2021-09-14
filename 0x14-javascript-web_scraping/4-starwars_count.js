e2551475ebd63bd2e8c06884228bb259600a1f9a
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-14 21:51:42 +0300

    Create 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+	#!/usr/bin/node
+
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const starwars = JSON.parse(body).results;
+  let c = 0;
+  for (let i = 0; i < starwars.length; i++) {
+    for (let j = 0; j < starwars[i].characters.length; j++) {
+      if (starwars[i].characters[j].includes('/18/')) {
+        c++;
+      }
+    }
+  }
+  console.log(c);
+});
