17b56d643604567f27009eeac4bd5b9e04853c14
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-14 21:55:48 +0300

    Create 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+	#!/usr/bin/node
+
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const dic = {};
+  const b = JSON.parse(body);
+  for (const i in b) {
+    const t = b[i];
+    if (t.completed) {
+      if (t.userId in dic) {
+        dic[t.userId] += 1;
+      } else {
+        dic[t.userId] = 1;
+      }
+    }
+  }
+  console.log(dic);
+});
