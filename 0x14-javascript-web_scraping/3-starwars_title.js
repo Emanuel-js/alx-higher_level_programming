a53548294d05386d95628b2e9a3012e8b6da0418
Emanuel-js <etioaman1960@gmail.com>
2021-09-14 17:08:02 +0000

    get data from api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  (err, res, body) => {
+    if (err) { throw err; }
+    console.log(JSON.parse(body).title);
+  });
