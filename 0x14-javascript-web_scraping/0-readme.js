a8eb15c21117e0918d423305fc1b233fafc3106d
Emanuel-js <etioaman1960@gmail.com>
2021-09-14 15:55:24 +0000

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const fs =require('fs');
+const fs = require('fs');
 
-fs.readFile(process.argv[2],'utf8',(err,data)=>{
-
-if(err)
-{
-console.log(err);
-}else
-{
-console.log(data);
-}
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
